aca906748176333e1efc38eefe50e630531d2e7a
johnnn-117 <hao.junh@northeastern.edu>
2022-12-11 17:22:27 -0800

    end2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 
+
 mongoose.connect('mongodb+srv://123:zxcvb@123.o3jb6fb.mongodb.net/?retryWrites=true&w=majority');
 //const CONNECTION_STRING = process.env.DB_CONNECTION_STRING||'mongodb://localhost:27017/tuiter'
 const db = mongoose.connection;
